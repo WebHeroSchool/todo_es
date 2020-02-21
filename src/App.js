361c6c11b0aa869045f4246839bd07886d02f997
Evgeniya Skripaleva <evganiya_ignaten@mail.ru>
2020-02-21 16:13:14 +0300

    task jsk done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const myVar = 'This is my variable';
+const myNumber = 22;
+const otherNumber = 11;
+const boolVar = true;
+
 function App() {
   return (
     <div className="App">
@@ -10,7 +15,13 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>Hello, World</p>
+        <p style={{color: 'lightblue'}}>Hello, World</p>
+        <p>{myVar}</p>
+        <p>{myNumber}</p>
+        <p>{myNumber + otherNumber}</p>
+        <p>{boolVar && 'This is logical operation'}</p>
+        <p>{boolVar ? 'This is true': 'This is false'}</p>
+        <p>{undefined}{null}{false}{true}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
