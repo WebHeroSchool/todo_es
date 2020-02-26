607dcdd09a078e4894280dd435867a440a8f91bc
Evgeniya Skripaleva <evganiya_ignaten@mail.ru>
2020-02-26 22:25:38 +0300

    task props fixed once more

--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -2,10 +2,9 @@ import React from 'react';
 import Item from '../Item/Item';
 
 
-const ItemList = ({ items }) => (items.map((items) => (
-    <ul>
-        <li><Item item={ items } /></li>
+const ItemList = ({ items }) =>  (<ul>
+    {items.map((items) => <li><Item item={ items } /></li>)}
     </ul>
-)));
+);
 
 export default ItemList;
