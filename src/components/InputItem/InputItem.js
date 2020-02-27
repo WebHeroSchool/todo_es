b9877f73d86e219923c78d1899dcb0d4a2f67c27
Evgeniya Skripaleva <evganiya_ignaten@mail.ru>
2020-02-27 16:24:48 +0300

    task Material UI done

--- src/components/InputItem/InputItem.js
+++ src/components/InputItem/InputItem.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import TextField from '@material-ui/core/TextField';
 
-const InputItem = () =>(
-    <input defaultValue={"Что нужно сделать?"}></input>
+const InputItem = () =>(<div>
+         <TextField
+             id="standard-helperText"
+             label="Что нужно сделать?"
+             defaultValue="Добавить задание"
+             helperText="Планируй свою работу и работай над своим планом."
+         />
+    </div>
 );
 
 
