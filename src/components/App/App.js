7631480aefc5d1796420b56e21f5250ad3f87dc9
Evgeniya Skripaleva <evganiya_ignaten@mail.ru>
2020-02-29 21:47:25 +0300

    task components-classes done

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,27 +4,30 @@ import InputItem from "../InputItem/InputItem";
 import Footer from "../Footer/Footer";
 import styles from './App.module.css';
 
-const App = () => {
-    const items = [
-        {
-            value: 'Написать новое приложение',
-            isDone: true
-        },
-        {
-            value: 'Прописать props',
-            isDone: true
-        },
-        {
-            value: 'сделать остальные дела',
-            isDone: false
-        }
-    ];
-    return (
-    <div className={styles.wrap}>
-        <h1>Список дел:</h1>
-        <InputItem />
-        <ItemList items={ items } />
-        <Footer count={ items.length }/>
-    </div>);
-};
+class App extends React.Component {
+    render() {
+        const items = [
+            {
+                value: 'Написать новое приложение',
+                isDone: true
+            },
+            {
+                value: 'Прописать props',
+                isDone: true
+            },
+            {
+                value: 'сделать остальные дела',
+                isDone: false
+            }
+        ];
+        return (
+            <div className={styles.wrap}>
+                <h1>Список дел:</h1>
+                <InputItem />
+                <ItemList items={ items } />
+                <Footer count={ items.length }/>
+            </div>);
+    }
+}
+
 export default App;
