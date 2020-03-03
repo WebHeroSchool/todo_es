80bfe4e47c739866035a3c1cbd7cb3c4e030bdea
Evgeniya Skripaleva <evganiya_ignaten@mail.ru>
2020-03-03 16:00:26 +0300

    task Data and Method Transfer is done

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -38,12 +38,17 @@ class App extends React.Component {
         this.setState({ items: newItemList });
     };
 
+    onClickDelete = id => {
+        const newItemList = this.state.items.filter(item => item.id !== id);
+        this.setState({ items: newItemList });
+    };
+
     render() {
         return (
             <div className={styles.wrap}>
                 <h1>Список дел:</h1>
                 <InputItem />
-                <ItemList items={ this.state.items } onClickDone={this.onClickDone}/>
+                <ItemList items={ this.state.items } onClickDone={this.onClickDone} onClickDelete={this.onClickDelete} />
                 <Footer count={ this.state.items.length }/>
             </div>);
     }
--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -5,7 +5,7 @@ import Checkbox from '@material-ui/core/Checkbox';
 import DeleteIcon from '@material-ui/icons/Delete';
 import IconButton from '@material-ui/core/IconButton';
 
-const Item = ({ value, isDone, onClickDone, id }) => (<div className={styles.flex}>
+const Item = ({ value, isDone, onClickDone, onClickDelete, id }) => (<div className={styles.flex}>
     <div>
         <Checkbox
             checked={isDone}
@@ -24,8 +24,8 @@ const Item = ({ value, isDone, onClickDone, id }) => (<div className={styles.fle
         </label>
     </div>
     <div>
-    <IconButton aria-label="delete" >
-    <DeleteIcon />
+    <IconButton aria-label="delete" onClick={() => onClickDelete(id)}>
+    <DeleteIcon  />
     </IconButton>
 </div>
 </div>);
