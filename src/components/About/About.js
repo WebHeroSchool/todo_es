8c197968ab6a7ac6de9fe726ccbac0767957772b
Evgeniya Skripaleva <evganiya_ignaten@mail.ru>
2020-03-16 03:14:28 +0300

    octokit import error

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,8 +1,30 @@
 import React from 'react';
 import styles from './About.module.css';
+import CardContent from '@material-ui/core/CardContent';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import Octokit from '@octokit/rest';
 
-const About = () => (
-    <h1 className={styles.wrap}>Обо мне:</h1>
-);
 
+const octokit = new Octokit();
+
+class About extends React.Component {
+    state = {
+        isLoading: true
+    };
+
+    componentDidMount() {
+        octokit.repos.listForUser({
+            username: 'aldeowl'
+        }).then(({ data }) => console.log(data));
+    }
+
+    render() {
+        const { isLoading } = this.state;
+        return(
+            <CardContent>
+                <h1 className={styles.wrap}>{ isLoading ? <CircularProgress /> : 'Обо мне:'}</h1>
+            </CardContent>
+        );
+    }
+}
 export default About;
